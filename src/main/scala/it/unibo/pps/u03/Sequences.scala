1c78181956cf3933ed0e0f251257b41cf8d3073a
Alessio Bifulco <alessio.bifulco@studio.unibo.it>
2026-03-16 18:01:05 +0100

    feat: skip, zip, concat, reverse, flatMap, min, evenIndices, contains & distinct

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -1,6 +1,9 @@
 package u03
 
 import u03.Optionals.Optional
+import u03.Optionals.Optional.*
+
+import scala.annotation.tailrec
 
 object Sequences: // Essentially, generic linkedlists
   
@@ -32,7 +35,17 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30], 0 => [10, 20, 30]
      * E.g., [], 2 => []
      */
-    def skip[A](s: Sequence[A])(n: Int): Sequence[A] = ???
+    def skip[A](s: Sequence[A])(n: Int): Sequence[A] = s match
+      case Cons(_, t) if n > 0 => skip(t)(n - 1)
+      case _                   => s
+
+    def skip2[A](s: Sequence[A])(n: Int): Sequence[A] =
+      @annotation.tailrec
+      def _skip(currentS: Sequence[A])(remaining: Int): Sequence[A] = currentS match
+        case Cons(_, t) if remaining > 0 => _skip(t)(remaining - 1)
+        case _ => currentS
+      _skip(s)(n)
+
 
     /*
      * Zip two sequences
@@ -40,7 +53,16 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10], [] => []
      * E.g., [], [] => []
      */
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(h, t), Cons(h2, t2)) => Cons((h, h2), zip(t, t2))
+      case _ => Nil()
+
+    def zip2[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] =
+      @tailrec
+      def _zip(f: Sequence[A], s: Sequence[B], acc: Sequence[(A, B)]): Sequence[(A, B)] = (f, s) match
+        case (Cons(h1, t1), Cons(h2, t2)) => _zip(t1, t2, Cons((h1, h2), acc))
+        case _ => acc
+      _zip(first, second, Nil())
 
     /*
      * Concatenate two sequences
@@ -48,7 +70,16 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10], [] => [10]
      * E.g., [], [] => []
      */
-    def concat[A](s1: Sequence[A], s2: Sequence[A]): Sequence[A] = ???
+    def concat[A](s1: Sequence[A], s2: Sequence[A]): Sequence[A] = s1 match
+      case Cons(h, t) => Cons(h, concat(t, s2))
+      case Nil() => s2
+
+    def concat2[A](s1: Sequence[A], s2: Sequence[A]): Sequence[A] =
+      @tailrec
+      def _concat(s1: Sequence[A], s2: Sequence[A], acc: Sequence[A]): Sequence[A] = s1 match
+        case Cons(h,t) => _concat(t, s2, Cons(h, acc))
+        case _ => acc
+      _concat(s1,s2,Nil())
 
     /*
      * Reverse the sequence
@@ -56,7 +87,18 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10] => [10]
      * E.g., [] => []
      */
-    def reverse[A](s: Sequence[A]): Sequence[A] = ???
+    def reverse2[A](s: Sequence[A]): Sequence[A] =
+      @annotation.tailrec
+      def _reverse(curr: Sequence[A], acc: Sequence[A]): Sequence[A] = curr match
+        case Cons(h, t) => _reverse(t, Cons(h, acc))
+        case Nil()      => acc
+      _reverse(s, Nil())
+
+    def reverse[A](s: Sequence[A]): Sequence[A] = s match
+      case Nil() => Nil()
+      case Cons(h, Nil()) => Cons(h, Nil())
+      case Cons(h,t) => concat(reverse(t), Cons(h, Nil()))
+
 
     /*
      * Map the elements of the sequence to a new sequence and flatten the result
@@ -64,35 +106,83 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30], calling with mapper(v => [v]) returns [10, 20, 30]
      * E.g., [10, 20, 30], calling with mapper(v => Nil()) returns []
      */
-    def flatMap[A, B](s: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+    def flatMap[A, B](s: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = s match
+      case Nil() => Nil()
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+
+    def flatMap2[A, B](s: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] =
+      @tailrec
+      def _flatMap[A,B](s: Sequence[A])(mapper: A => Sequence[B])(acc: Sequence[B]): Sequence[B] = s match
+        case Nil() => acc
+        case Cons(h,t) => _flatMap(t)(mapper)(concat(acc, mapper(h)))
+      _flatMap(s)(mapper)(Nil())
+
 
     /*
      * Get the minimum element in the sequence
      * E.g., [30, 20, 10] => 10
      * E.g., [10, 1, 30] => 1
      */
-    def min(s: Sequence[Int]): Optional[Int] = ???
+
+    def min (s: Sequence[Int]): Optional[Int] = s match
+      case Nil() => Empty()
+      case Cons(h,t) if orElse(min(t), h) >= h => Just(h)
+      case Cons(h,t) => min(t)
+
+    def min2(s: Sequence[Int]): Optional[Int] =
+      @annotation.tailrec
+      def _min(curr: Sequence[Int], acc: Optional[Int]): Optional[Int] = curr match
+        case Nil() => acc
+        case Cons(h, t) =>
+          val nextMin = acc match
+            case Just(v) => if (h < v) Just(h) else Just(v)
+            case Empty() => Just(h)
+          _min(t, nextMin)
+      _min(s, Empty())
 
     /*
      * Get the elements at even indices
      * E.g., [10, 20, 30] => [10, 30]
      * E.g., [10, 20, 30, 40] => [10, 30]
      */
-    def evenIndices[A](s: Sequence[A]): Sequence[A] = ???
+    def evenIndices[A](s: Sequence[A]): Sequence[A] = s match
+      case Nil() => Nil()
+      case Cons(h,Cons(_, t)) =>  Cons(h, evenIndices(t))
+      case Cons(h, Nil()) => Cons(h, Nil())
+
+    def evenIndices2[A](s: Sequence[A]): Sequence[A] = s match
+      case Nil() => Nil()
+      case Cons(h,t) => Cons(h, evenIndices2(skip(t)(1)))
 
     /*
      * Check if the sequence contains the element
      * E.g., [10, 20, 30] => true if elem is 20
      * E.g., [10, 20, 30] => false if elem is 40
      */
-    def contains[A](s: Sequence[A])(elem: A): Boolean = ???
+    @tailrec
+    def contains[A](s: Sequence[A])(elem: A): Boolean = s match
+      case Cons(h,_) if h == elem => true
+      case Cons(_,t) => contains(t)(elem)
+      case _ => false
 
     /*
      * Remove duplicates from the sequence
      * E.g., [10, 20, 10, 30] => [10, 20, 30]
      * E.g., [10, 20, 30] => [10, 20, 30]
      */
-    def distinct[A](s: Sequence[A]): Sequence[A] = ???
+
+    def distinct[A](s: Sequence[A]): Sequence[A] = s match
+      case Nil() => Nil()
+      case Cons(h, t) => Cons(h, distinct(filter(t)(_ != h)))
+
+    def distinct2[A](s: Sequence[A]): Sequence[A] =
+      @tailrec
+      def _distinct(s: Sequence[A], acc: Sequence[A]): Sequence[A] = s match
+        case Nil() => acc
+        case Cons(h, t) => if contains(acc)(h) then _distinct(t, acc) else _distinct(t, concat(acc, Cons(h, Nil())))
+      _distinct(s, Nil())
+
+
 
     /*
      * Group contiguous elements in the sequence
